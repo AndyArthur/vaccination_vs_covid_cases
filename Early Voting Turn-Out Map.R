daa0075c1d38fa19605a348f0a14a66e877b157d
AndyArthur <95251802+AndyArthur@users.noreply.github.com>
2022-11-01 16:45:49 -0400

    Update Early Voting Turn-Out Map.R

--- Early Voting Turn-Out Map.R
+++ Early Voting Turn-Out Map.R
@@ -31,7 +31,7 @@ cls <- wacolors::wa_pal('ferries',n=5)[4]
 nyco %>% 
   inner_join(tom, by=c('NAME'='county')) %>%
   inner_join(ce, by=c('NAME'='county')) %>%
-  mutate(pv = `ev`/TOTAL, pv = cut(pv, breaks=seq(0,1,0.01), labels=scales::percent(seq(0,0.99,0.01), accuracy=1))) %>%
+  mutate(pv = `ev`/TOTAL, pv = cut(pv, breaks=seq(0,1,0.01), labels=scales::percent(seq(0,0.99,0.01), accuracy = 1))) %>%
   ggplot() +
   geom_sf(aes(fill=pv), color='white') +
   wacolors::scale_fill_wa_d(palette = "ferries", guide = "legend") +
@@ -100,6 +100,45 @@ nyco %>%
   ) 
 
 fn <- str_c('early-vote-scale')
+ggsave(paste('~/Desktop/',fn,'.jpg',sep=''), width=1920, height=1200, units='px', dpi=120, device = grDevices::jpeg)
+ggsave(paste('~/Desktop/',fn,'.svg',sep=''), width=1920, height=1200, units='px', dpi=120, device = grDevices::svg)
+system(paste('scour ~/Desktop/',fn,'.svg ~/Desktop/',fn,'.svgz',sep=''))
+unlink(str_c('~/Desktop/',fn,'.svg'))
+
+
+cls <- 'red'
+nyco %>% 
+  inner_join(tom, by=c('NAME'='county')) %>%
+  inner_join(ce, by=c('NAME'='county')) %>%
+  st_transform(3857) %>%
+  ggplot() + 
+  geom_sf(aes(fill=DEM/(DEM+REP), alpha=ev)) +
+  scale_fill_gradient(name='', low='red', high='blue', labels=scales::label_percent(), limit=c(0,1)) +
+  coord_sf(expand=F, crs=3857) +
+  theme_void() +
+  labs(title = str_c('<span style=\"font-size: 80px; color: ',cls,
+                     '\">Early Voters</span><br /> Color by active enrollment, saturation based on early votes through ', format(today()%m-%days(1), format='%A %b %-d.')),
+       y = "",
+       x = "",
+       tag=paste('Andy Arthur,', format(Sys.Date(), format="%-m/%-d/%y"),'\nSource: NYS Active Enrollments and this spreadsheet by @cinyc9\n',uri),
+       fill = "")  +
+  theme(
+    legend.key.height = unit(0.35,'cm'),
+    legend.key.width = unit(3,'cm'),
+    legend.position = c(0.05,0.17),
+    legend.direction = 'horizontal',
+    legend.justification = 0,
+    text= element_text(family='Lato',size=14),
+    plot.title=element_textbox(halign = 0.5, hjust=0, face='bold',size=25, margin=unit(c(25,0,5,0),'pt'), maxheight=0, width=0.4),
+    plot.background = element_rect(fill = "white", color="white"),
+    plot.subtitle=element_textbox(hjust=0.5, halign=0.5, margin=unit(c(5,0,5,0),'pt')),
+    plot.tag=element_text(size=10,hjust=0, color='#555555'),
+    plot.margin = unit(c(1,1,1,1), 'lines'),
+    plot.tag.position = c(0.0,0.01),
+  ) +
+  guides(alpha=guide_none())
+
+fn <- str_c('early-vote-alpha')
 ggsave(paste('~/Desktop/',fn,'.jpg',sep=''), width=1920, height=1200, units='px', dpi=120, device = grDevices::jpeg)
 ggsave(paste('~/Desktop/',fn,'.svg',sep=''), width=1920, height=1200, units='px', dpi=120, device = grDevices::svg)
 system(paste('scour ~/Desktop/',fn,'.svg ~/Desktop/',fn,'.svgz',sep=''))
